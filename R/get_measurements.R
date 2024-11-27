1c723e1202bdb6c2beb6c7308ad3421d69f27803
Dimitri Falk <dimitri.falk1@gmail.com>
2024-11-27 14:32:24 +0100

    enh(get_measurements):. allow tibble
    #8

--- R/get_measurements.R
+++ R/get_measurements.R
@@ -1,15 +1,20 @@
 #' Get water level or discharge measurements
 #'
-#' @param x character. Station ID.
+#' @param x Tibble as provided by `get_gauges()`.
 #' @param discharge logical. Retrieve water level or discharge data?
 #'
-#' @return xts object.
+#' @return List of xts objects.
 #' @export
 #'
 #' @examples
-#' get_measurements("10104")
+#' station <- get_gauges() |> dplyr::filter(id == "10103")
 #'
-#' get_measurements("10103", discharge = TRUE)
+#' get_measurements(station)
+#' get_measurements(station, discharge = TRUE)
+#'
+#' stations <- get_gauges() |> dplyr::filter(waterbody == "Hüller Bach")
+#'
+#' get_measurements(stations)
 get_measurements <- function(x = NULL,
                              discharge = FALSE) {
 
@@ -17,30 +22,38 @@ get_measurements <- function(x = NULL,
 
   base_url <- "https://pegel.eglv.de/measurements/"
 
+  # set relevant unit
   if (discharge == FALSE) {
 
-    url <- paste0(base_url,
-                  "?serial=",
-                  x,
-                  "&unit_name=Wasserstand")
-
-    raw <- jsonlite::fromJSON(url)[["gauge_measurements"]]
-
-    meas <- xts::xts(as.numeric(raw[, 2]),
-                     order.by = strptime(raw[, 1], format = "%Y-%m-%dT%H:%M:%SZ", tz = "etc/GMT-1"))
+    unit <- "Wasserstand"
 
   } else {
 
+    unit <- "Durchfluss"
+  }
+
+  # iterate over individual stations
+  ids <- x[["id"]]
+
+  n <- length(ids)
+
+  xtslist <- list()
+
+  for (i in 1:n) {
+
     url <- paste0(base_url,
                   "?serial=",
-                  x,
-                  "&unit_name=Durchfluss")
+                  ids[i],
+                  "&unit_name=",
+                  unit)
 
     raw <- jsonlite::fromJSON(url)[["gauge_measurements"]]
 
     meas <- xts::xts(as.numeric(raw[, 2]),
                      order.by = strptime(raw[, 1], format = "%Y-%m-%dT%H:%M:%SZ", tz = "etc/GMT-1"))
+
+    xtslist[[i]] <- meas
   }
 
-  meas
+  xtslist
 }
